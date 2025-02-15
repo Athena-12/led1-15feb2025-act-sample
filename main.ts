a84e41a4760ba82cec67aa7c55c7885830494cbc
Athena-12 <cabugon.eloisa@gmail.com>
2025-02-15 12:56:51 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,36 @@
-basic.showIcon(IconNames.Heart)
-basic.pause(2000)
-basic.clearScreen()
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(200)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(200)
+    }
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 1; index++) {
+        basic.showNumber(1)
+        basic.showNumber(2)
+        basic.showNumber(3)
+        basic.showString("ACT RoCkZ!!")
+    }
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . . . .
+            . # # . .
+            # # # . .
+            . # # # #
+            . # # # .
+            `)
+        basic.pause(200)
+        basic.showIcon(IconNames.Duck)
+        basic.pause(200)
+    }
+    basic.clearScreen()
+})
 basic.forever(function () {
 	
 })
